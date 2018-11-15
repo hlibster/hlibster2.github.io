a8dbc53fe13016fc066277a8db9a7281e6ead4e0
Hlibster <hlib.bryskin@gmail.com>
2018-11-15 15:49:35 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+
 
 function inputLength() {
 	return input.value.length;
@@ -32,6 +37,11 @@ function addListAfterKeypress(event) {
 function addConsoleFeedback() {
 	console.log("working");
 }
+function setGradient() {
+	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+	css.textContent = "Current css background: " + body.style.background + ";"
+}
+
 
 ul.onclick=function(event){
 	var target=event.target;
@@ -47,3 +57,11 @@ ul.onclick=function(event){
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+
+
+
